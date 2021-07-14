e4a27c93a100847f6d7cbc817441168e2bd44364
Ardiant Utomo <ardiantutomo@gmail.com>
2021-07-14 20:01:29 +0700

    added variables example

---
+++ variables.example.go
@@ -0,0 +1,11 @@
+package variables
+
+var (
+	KAFKA_HOST  = "localhost:9092"
+	KAFKA_GROUP = "-"
+	DB_USER = "postgres"
+	DB_PASS = "root"
+	DB_HOST = "localhost"
+	DB_PORT = "5432"
+	DB_DBNAME = "-"
+)
